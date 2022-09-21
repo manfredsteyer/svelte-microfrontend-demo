30c334974db91d5c63518e7e02377f241e385d7e
gioboa <giorgiob.boa@gmail.com>
2022-09-21 18:43:37 +0200

    cleanup remote config

--- remote/vite.config.ts
+++ remote/vite.config.ts
@@ -3,25 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { federation } from './module-federation/vite-federation-plugin';
 import { esBuildAdapter } from './module-federation/esbuild-adapter';
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 
 export default defineConfig(async ({command}) => ({
-	server: {
-		fs: {
-			// IMPORTANT: Make sure to allow access to all shared libs
-			//	esp. if they are not directly below the project's 
-			//	directory like here. Also, don't forget '.'
-			// strict: false
-			allow:[
-				'.',
-				'../shared', 
-			]
-		}
-	},
 	plugins: [
-		esbuildCommonjs(['colormap']),
-		// IMPORTANT: Don't use this plugin!
-		// tsconfigPaths(),
+		tsconfigPaths(),
 		federation({
 			options: {
 				workspaceRoot: __dirname,
